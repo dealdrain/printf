6f7435c8fcef5f16c93616da8bbd7ed07fba959a
dealdrain <dealdrain@yahoo.com>
2023-07-24 07:44:29 -0700

    updat

--- print_format.c
+++ print_format.c
@@ -11,53 +11,54 @@
 int _print_format(const char *format, va_list args)
 {
 	int count = 0;
-	int i = 0;
+	int x = 0;
 
-	while (format && format[i])
+	while (format && format[x])
 	{
-		if (format[i] == '%')
+		if (format[x] == '%')
 		{
-			if (format[i + 1] == '\0')
+			if (format[x + 1] == '\0')
 				return (-1);
 
-			i++;
+			x++;
 
-			while (format[i] == ' ')
-				i++;
+			while (format[x] == ' ')
+				x++;
 
-			if (format[i] == '%')
-				count += _write(format[i]);
+			if (format[x] == '%')
+				count += _write(format[x]);
 
-			if (_validate_char(format[i]) == 0)
+			if (_validate_char(format[x]) == 0)
 			{
-				count = _print_invalid_spec(format[i - 1], format[i], count);
+				count = _print_invalid_spec(format[x - 1], format[x], count);
 			}
 			else
 			{
-				count += _print_spec(format[i], args);
+				count += _print_spec(format[x], args);
 			}
 		}
 		else
 		{
-			count += _write(format[i]);
+			count += _write(format[x]);
 		}
 
-		i++;
+		x++;
 	}
 
 	return (count);
 }
 
 /**
-  * _print_spec - Prints a valid specifier
-  * @format: The specifier to prints
-  * @args: A list of variadic arguments
+  * _print_spec - Print specifier
+  * @format: specifier to print
+  * @args: list of vari args
   *
-  * Return: The length of the specifier
+  * Return: length of the specifier
   */
+
 int _print_spec(char format, va_list args)
 {
-	int i  = 0, length = 0;
+	int x  = 0, length = 0;
 	spc_dt _types[] = {
 		{"c", _print_a_char},
 		{"s", _print_a_string},
@@ -67,25 +68,26 @@ int _print_spec(char format, va_list args)
 		{NULL, NULL}
 	};
 
-	while (_types[i].specifier)
+	while (_types[x].specifier)
 	{
 		if (*_types[i].specifier == format)
-			length = _types[i].f(args);
+			length = _types[x].f(args);
 
-		i++;
+		x++;
 	}
 
 	return (length);
 }
 
 /**
-  * _print_invalid_spec - Prints a invalid specifier
-  * @prev_format: The previous specifier of actual specifier
-  * @format: The specifier to prints
-  * @count: The current count before prints invalid specifiers
+  * _print_invalid_spec - Print invalid specifier
+  * @prev_format: The prev specifier
+  * @format: specifier to print
+  * @count: current count before print- invalid specifiers
   *
-  * Return: The current count after prints invalid specifiers
+  * Return: current count after prints- invalid specifiers
   */
+
 int _print_invalid_spec(char prev_format, char format, int count)
 {
 	count += _write('%');
@@ -104,21 +106,22 @@ int _print_invalid_spec(char prev_format, char format, int count)
 }
 
 /**
-  * _validate_char - validate the type
-  * @_type: character to be comparate
+  * _validate_char - type validation
+  * @_type: char
   *
   * Return: 1 if char is equal to a type
   */
+
 int _validate_char(char _type)
 {
 	char _types[] = {'c', 's', 'd', 'i', 'b', '%'};
-	int i = 0;
+	int p = 0;
 
-	while (_types[i])
+	while (_types[p])
 	{
-		if (_types[i] == _type)
+		if (_types[p] == _type)
 			return (1);
-		i++;
+		p++;
 	}
 	return (0);
 }
