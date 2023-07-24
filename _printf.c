a1e50a407043126b47592bddf2115c9788f13537
dealdrain <dealdrain@yahoo.com>
2023-07-24 07:12:27 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,55 +1,24 @@
-#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-#include <stddef.h>
 
 /**
- * _printf - recreate a printf function
- * @format: string
- * Return: number of char
+ * _printf - function my printf
+ * @format: string whit format to print
  *
+ * Return: number of chars that print
  */
-
 int _printf(const char *format, ...)
 {
-	if (format != NULL)
-	{
-		int count = 0, x;
-		int (*m)(va_list);
-		va_list args;
+	va_list args;
+	int length = 0;
 
-		va_start(args, format);
-		x = 0;
-		if (format[0] == '%' && format[1] == '\0')
-			return (-1);
-		while (format != NULL && format[x] != '\0')
-		{
-			if (format[x] == '%')
-			{
-				if (format[x + 1] == '%')
-				{
-					count += _putchar(format[x]);
-					x += 2;
-				}
-				else
-				{
-					m = get_func(format[x + 1]);
-					if (m)
-						count += m(args);
-					else
-						count = _putchar(format[x]) + _putchar(format[x + 1]);
-					x += 2;
-				}
-			}
-			else
-			{
-				count += _putchar(format[x]);
-				x++;
-			}
-		}
-		va_end(args);
-		return (count);
-	}
-	return (-1);
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	length = _print_format(format, args);
+	va_end(args);
+	return (length);
 }
