bb3cad57fcc9f933a32587f62a70079fb014e275
dealdrain <dealdrain@yahoo.com>
2023-07-25 04:00:05 -0700

    update

--- _printf.c
+++ _printf.c
@@ -3,23 +3,25 @@
 #include "main.h"
 
 /**
- * _printf - function my printf
- * @format: string whit format to print
+ * _printf - function printf
+ * @format: string format to print
  *
- * Return: number of chars that print
+ * Return: no. of chars that print
  */
 
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int length = 0;
+	int x;
+	x = 0;
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
 
-	length = _print_format(format, args);
+	x = _print_format(format, args);
 	va_end(args);
-	return (length);
+	
+	return (x);
 }
